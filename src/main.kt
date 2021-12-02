04f68431b68f23ef0154fce8511db855707f4550
temurakami <spellman23@gmail.com>
2021-12-02 01:02:32 -0800

    changes for the 3-item window

--- src/main.kt
+++ src/main.kt
@@ -5,21 +5,27 @@ fun main() {
     println("Hello World")
     val filename = "input"
     val lines = File(filename).readLines()  // returns List<String>
+    // Create 3-sliding window list
+    val finalwindow: MutableList<Int> = mutableListOf()
+    for (i in lines.indices){
+        if (i < 2){
+            continue
+        }
+        finalwindow.add(lines[i].toInt() + lines[i-1].toInt() + lines[i-2].toInt())
+    }
     var inc = 0
-    var prev = ""
-    for (line in lines)
+    var prev = 0
+    for (value in finalwindow)
     {
-        if (line == ""){
+        //println(value.toString())
+        if (prev == 0){
+            prev = value
             continue
         }
-        if (prev == ""){
-            prev = line
-            continue
-        }
-        if (prev.toInt() < line.toInt()){
+        if (prev < value){
             inc++
         }
-        prev = line
+        prev = value
     }
     println("increases: "+inc.toString())
 }
