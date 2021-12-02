01f0e3245e4fabf7fd8342fef99bf3fa8a3094af
temurakami <spellman23@gmail.com>
2021-12-02 00:47:06 -0800

    initial version

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,21 @@
+import java.io.File
+
+
 fun main() {
     println("Hello World")
-} 
+    val filename = "test"
+    val lines = File(filename).readLines()  // returns List<String>
+    var inc = 0
+    var prev = ""
+    for (line in lines)
+    {
+        if (prev == ""){
+            prev = line
+            continue
+        }
+        if (prev.toInt() < line.toInt()){
+            inc++
+        }
+    }
+    println("increases: "+inc.toString())
+}
