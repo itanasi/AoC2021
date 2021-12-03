b7d7e7c31e93d5f6d1ad12c7ac7386d928cd1383
temurakami <spellman23@gmail.com>
2021-12-03 00:11:31 -0800

    Clean up using IntelliJ Suggestions

--- src/main.kt
+++ src/main.kt
@@ -1,12 +1,11 @@
 import java.io.File
-import javax.print.DocFlavor
 import kotlin.math.pow
 
 
 fun binToDec(input: String): Int
 {
     var dec = 0
-    for (bit in 0..input.length-1){
+    for (bit in input.indices){
         if (input[bit].toString() == "1"){
             dec += 2.toDouble().pow(input.length-bit-1).toInt()
         }
@@ -19,10 +18,10 @@ fun main() {
     val filename = "input"
     val length = 12
     val lines = File(filename).readLines().asSequence()  // returns List<String>
-    var count = MutableList(length) {i -> 0}
+    var count = MutableList(length) {0}
     var prefix = ""
     var o2 = ""
-    for (bit in 0..length-1) {
+    for (bit in 0 until length) {
         val numLines = lines.filter{prefix == it.take(bit)}.count()
         println(numLines)
         if (numLines == 1){
@@ -32,23 +31,22 @@ fun main() {
         for (line in lines.filter{prefix == it.take(bit)}){
             count[bit] = count[bit] + line[bit].toString().toInt()
         }
-        if(count[bit].toFloat() >= numLines.toFloat()/2f){
-            prefix += "1"
+        prefix += if(count[bit].toFloat() >= numLines.toFloat()/2f){
+            "1"
         }else{
-            prefix += "0"
+            "0"
         }
         println("count: $count")
         println("prefix: $prefix")
         o2 = prefix
     }
     println("o2: $o2")
-    val o2_dec = binToDec(o2)
-    println("o2_dec: $o2_dec")
+    val o2dec = binToDec(o2)
 
-    count = MutableList(length) {i -> 0}
+    count = MutableList(length) {0}
     prefix = ""
     var co2 = ""
-    for (bit in 0..length-1) {
+    for (bit in 0 until length) {
         val numLines = lines.filter{prefix == it.take(bit)}.count()
         println(numLines)
         if (numLines == 1){
@@ -58,18 +56,18 @@ fun main() {
         for (line in lines.filter{prefix == it.take(bit)}){
             count[bit] = count[bit] + line[bit].toString().toInt()
         }
-        if(count[bit].toFloat() < numLines.toFloat()/2f){
-            prefix += "1"
+        prefix += if(count[bit].toFloat() < numLines.toFloat()/2f){
+            "1"
         }else{
-            prefix += "0"
+            "0"
         }
         println("count: $count")
         println("prefix: $prefix")
         co2 = prefix
     }
     println("co2: $co2")
-    val co2_dec = binToDec(co2)
-    println("o2_dec: $o2_dec  co2_dec: $co2_dec")
-    val power = o2_dec * co2_dec
+    val co2dec = binToDec(co2)
+    println("o2dec: $o2dec  co2dec: $co2dec")
+    val power = o2dec * co2dec
     println("power: $power")
 }
