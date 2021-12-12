6839734e9ff80eda71732e39a4b16c6e5fdb4bc9
temurakami <spellman23@gmail.com>
2021-12-11 20:53:31 -0800

    The power of recursion compels you

--- src/main.kt
+++ src/main.kt
@@ -38,7 +38,37 @@ This costs a total of 37 fuel. This is the cheapest possible outcome; more expen
 
 Determine the horizontal position that the crabs can align to using the least fuel possible. How much fuel must they spend to align to that position?
 
+--- Part Two ---
+
+The crabs don't seem interested in your proposed solution. Perhaps you misunderstand crab engineering?
+
+As it turns out, crab submarine engines don't burn fuel at a constant rate. Instead, each change of 1 step in horizontal position costs 1 more unit of fuel than the last: the first step costs 1, the second step costs 2, the third step costs 3, and so on.
+
+As each crab moves, moving further becomes more expensive. This changes the best horizontal position to align them all on; in the example above, this becomes 5:
+
+    Move from 16 to 5: 66 fuel
+    Move from 1 to 5: 10 fuel
+    Move from 2 to 5: 6 fuel
+    Move from 0 to 5: 15 fuel
+    Move from 4 to 5: 1 fuel
+    Move from 2 to 5: 6 fuel
+    Move from 7 to 5: 3 fuel
+    Move from 1 to 5: 10 fuel
+    Move from 2 to 5: 6 fuel
+    Move from 14 to 5: 45 fuel
+
+This costs a total of 168 fuel. This is the new cheapest possible outcome; the old alignment position (2) now costs 206 fuel instead.
+
+Determine the horizontal position that the crabs can align to using the least fuel possible so they can make you an escape route! How much fuel must they spend to align to that position?
+
  */
+
+fun distanceToFuel(distance: Int): Int
+{
+    if (distance == 0)
+        return 0
+    return distanceToFuel(distance-1)+distance
+}
 
 fun main() {
     println("Hello World")
@@ -48,9 +78,9 @@ fun main() {
     val crabPos = lines.first().split(",").map{it.toInt()}
     var minPos = 0
     // Default to moving all to position 0
-    var minFuel = crabPos.sumOf { it }
+    var minFuel = crabPos.sumOf { distanceToFuel(it) }
     for (pos in 1..crabPos.maxOf{ it }){
-        val fuel = crabPos.map{ abs(it - pos) }.sumOf { it }
+        val fuel = crabPos.map{ distanceToFuel(abs(it - pos)) }.sumOf { it }
         if (fuel < minFuel){
             minPos = pos
             minFuel = fuel
